fba1e0840970e693bbb8fc72ba59e6a16b399c24
Adrian Nguyen <adriannguyen8815@gmail.com>
2026-01-13 19:43:37 -0600

    u

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,33 +1,82 @@
-import formbackground from "../Assets/formbackground.jpg"
+import formbackground from "../Assets/formbackground.jpg";
 
 export default function WorkOrderForm() {
-    return (
-        <div className="form-page-contain">
-            <form className="form-contain">
-            <div className="form-top">
-                <h1>Submit a Work Order </h1>
-                <p>Have an issue? Fill out the form and i can help!</p>
-            </div>
-            <div className="form-middle">
-                <span>Select Issue Type</span>
-                <Select className="issue-type-dropdown">
-                    <option value="setup">Setup</option>
-                    <option value="troubleshooting">Troubleshooting</option>
-                    <option value="printer-issues">Printer Issues</option>
-                    <option value="wifi-problems">Wi-Fi Problems</option>
-                    <option value="other">Other</option>
-                </Select>
-                <span>Describe your issue</span>
-                <textarea className="issue-description" placeholder="Please provide a detailed description of the issue you are facing..."></textarea>
-                <span>Contact Information</span>
-                <input type="text" className="contact-info" placeholder="Your email or phone number"/>
-                <button type="submit" className="submit-button-form">Submit Work Order</button>
-            </div>
-            <div className="form-bottom">
-
-            </div>
-        </form>
+  return (
+    <div className="form-page-contain">
+      <form className="form-contain">
+        <div className="form-top">
+          <h1>Submit a Work Order </h1>
+          <p>Have an issue? Fill out the form and i can help!</p>
         </div>
-        
-    )
+
+        <div className="form-info-container">
+          <div className="form-info-left">
+            <div className="form-info-type">
+              <span>Select Issue Type</span>
+              <select className="issue-type-dropdown">
+                <optgroup label="Network & Internet">
+                  <option value="wifi">Wi-Fi Connectivity</option>
+                  <option value="ethernet">Ethernet/LAN</option>
+                  <option value="modem">Modem/ISP Issues</option>
+                  <option value="network-setup">Network Setup</option>
+                </optgroup>
+
+                <optgroup label="Hardware">
+                  <option value="printer">Printer Issues</option>
+                  <option value="computer">Computer/Laptop</option>
+                </optgroup>
+
+                <optgroup label="Software">
+                  <option value="os">OS Issues</option>
+                  <option value="virus">Virus/Malware</option>
+                  <option value="application">System/Application Errors</option>
+                  <option value="performance">Performance Issues</option>
+                </optgroup>
+
+                <optgroup label="Other">
+                  <option value="other">Other (Specify Below)</option>
+                </optgroup>
+              </select>
+            </div>
+
+            <div className="form-info-description">
+              <span>Describe your issue</span>
+              <textarea
+                className="issue-description"
+                placeholder="Please provide a detailed description of the issue you are facing..."
+              ></textarea>
+            </div>
+          </div>
+          <div className="form-info-right">
+            <span>Upload any images</span>
+            <input type="file" className="image-upload" multiple />
+          </div>
+        </div>
+
+        <div className="form-contact">
+          <div className="form-contact-top">
+            <span>Contact Info</span>
+          </div>
+          <div className="form-contact-bottom">
+            <input type="file" className="image-upload" multiple />
+            <input
+              type="text"
+              className="contact-info"
+              placeholder="First Name"
+            />
+            <input
+              type="text"
+              className="contact-info"
+              placeholder="Last Name"
+            />
+            <input
+              type="email"
+              className="contact-info"
+              placeholder="Email Address"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
